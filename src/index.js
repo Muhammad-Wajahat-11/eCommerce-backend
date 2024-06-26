2259efd8151b3186931d52f7a04b1d06757ca605
Muhammad-Wajahat-11 <muhammadwajahat381@gmail.com>
2024-06-27 02:42:35 +0500

    express server on  get & post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import connectDB from "./db/db.index.js";
-
+import { app } from './app.js';
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at ${process.env.PORT}`);
+        })
+
+        app.get('/', (req, res) => {
+            res.send('Hello Developer')
+        })
+
+        app.post('/', (req, res) => {
+            res.send('POST request to homepage')
+        })
+    })
+    .catch((err) => {
+        console.error("DB Connection Failed", err);
+    })
